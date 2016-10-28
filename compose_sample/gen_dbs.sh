8118edd95877fb3716d243cb313e31b0e4371df5
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-28 11:26:00 +0100

    first fully working version that migrates and dump databases properly

--- compose_sample/gen_dbs.sh
+++ compose_sample/gen_dbs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-DATABASES="default mysql"
+DATABASES="default"
 
 for db in $DATABASES
 do ./manage.py migrate --database $db
 done
 
-pip install --trusted-host 172.17.42.1 --extra-index-url http://172.17.42.1:8001 dev-tools==0.1.3
+pip install --trusted-host 172.17.42.1 --extra-index-url http://172.17.42.1:8001 dev-tools==0.1.4
 
-mkdir db_dumps;
+# mkdir db_dumps;
 
-iw-db dump_all -c /dbs.yml db_dumps/
+mkdir /db_dumps; iw-db dump_all -c /dbs.yml /db_dumps/
