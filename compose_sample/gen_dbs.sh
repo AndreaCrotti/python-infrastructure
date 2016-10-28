617d33cf8dc5204c1048f90e18865b75489e4628
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-28 11:53:05 +0100

    get it finally all working fine

--- compose_sample/gen_dbs.sh
+++ compose_sample/gen_dbs.sh
@@ -6,8 +6,6 @@ for db in $DATABASES
 do ./manage.py migrate --database $db
 done
 
-pip install --trusted-host 172.17.42.1 --extra-index-url http://172.17.42.1:8001 dev-tools==0.1.4
-
-# mkdir db_dumps;
+pip install --trusted-host 172.17.42.1 --extra-index-url http://172.17.42.1:8001 dev-tools==0.1.5
 
 mkdir /db_dumps; iw-db dump_all -c /dbs.yml /db_dumps/
